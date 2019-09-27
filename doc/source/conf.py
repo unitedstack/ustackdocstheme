ff2282a0114a5329f29ba756b3635bc000622419
William Huang <ai.william@outlook.com>
2019-09-27 12:46:56 +0800

    update ustackdocs theme options
    Signed-off-by: William Huang <ai.william@outlook.com>

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -1,21 +1,9 @@
 # -*- coding: utf-8 -*-
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#    http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
-# implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 import os
 import sys
 
 sys.path.insert(0, os.path.abspath('../..'))
+
 # -- General configuration ----------------------------------------------------
 
 # Add any Sphinx extension module names here, as strings. They can be
@@ -40,7 +28,7 @@ master_doc = 'index'
 project = u'ustackdocstheme'
 copyright = u'2019, William Huang'
 
-# openstackdocstheme options
+# ustackdocstheme options
 repository_name = 'docs/ustackdocstheme'
 html_last_updated_fmt = '%Y-%m-%d %H:%M'
 
@@ -62,19 +50,8 @@ pygments_style = 'sphinx'
 # html_theme = '_theme'
 # html_static_path = ['static']
 html_theme = 'ustackdocs'
+html_theme_options = {
+    'analytics_tracking_code': False
+}
 
-# Output file base name for HTML help builder.
-htmlhelp_basename = '%sdoc' % project
-
-# Grouping the document tree into LaTeX files. List of tuples
-# (source start file, target name, title, author, documentclass
-# [howto/manual]).
-latex_documents = [
-    ('index',
-     '%s.tex' % project,
-     u'%s Documentation' % project,
-     u'OpenStack Developers', 'manual'),
-]
-
-# Example configuration for intersphinx: refer to the Python standard library.
-#intersphinx_mapping = {'http://docs.python.org/': None}
+# -- End options for HTML output ----------------------------------------------
