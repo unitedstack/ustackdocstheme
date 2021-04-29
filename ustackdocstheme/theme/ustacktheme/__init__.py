6f64242187e8ccd7e30d82a39b25dc1227346b7e
yangjunjie <yangjunjie@unitedstack.com>
2021-04-29 20:40:39 +0800

    add header

---
+++ ustackdocstheme/theme/ustacktheme/__init__.py
@@ -0,0 +1,13 @@
+import os
+
+# http://www.sphinx-doc.org/en/master/theming.html#distribute-your-theme-as-a-python-package
+
+
+def setup(app):
+    app.add_html_theme(
+        'ustacktheme',
+        os.path.abspath(os.path.dirname(__file__)),
+    )
+    return {
+        'parallel_read_safe': True,
+    }
